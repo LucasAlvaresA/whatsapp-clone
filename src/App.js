2f45c3c3bad3bca2b411e1dc61c49d73fb3e7bdd
Lucas <dexterfail@hotmail.com>
2021-04-20 11:17:40 -0300

    template criado

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import ChatListItem from "./components/ChatListItem";
 import ChatIntro from "./components/ChatIntro";
 import ChatWindow from "./components/ChatWindow";
+import NewChat from  "./components/NewChat";
 
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
@@ -12,64 +13,76 @@ import SearchIcon from '@material-ui/icons/Search';
 
 export default function App() {
 
- const [chatlist, setChatList] = useState([
-   {chatId: 1, title: "Fulano de Tal", image: "https://www.w3schools.com/howto/img_avatar2.png"},
-   {chatId: 2, title: "Fulano de Tal", image: "https://www.w3schools.com/howto/img_avatar2.png"},
-   {chatId: 3, title: "Fulano de Tal", image: "https://www.w3schools.com/howto/img_avatar2.png"},
-   {chatId: 4, title: "Fulano de Tal", image: "https://www.w3schools.com/howto/img_avatar2.png"},
- ]);
- const [activeChat, setActiveChat] = useState({});
- const [user, setUser] = useState({
-   id:1234,
-   avatar: "https://www.w3schools.com/howto/img_avatar2.png",
-   name: "Usuario Logado"
- });
+  const [chatlist, setChatList] = useState([
+    {chatId: 1, title: "Usuario 1", image: "https://www.w3schools.com/howto/img_avatar2.png"},
+    {chatId: 2, title: "Usuario 2", image: "https://www.w3schools.com/howto/img_avatar2.png"},
+    {chatId: 3, title: "Usuario 3", image: "https://www.w3schools.com/howto/img_avatar2.png"},
+    {chatId: 4, title: "Usuario 4", image: "https://www.w3schools.com/howto/img_avatar2.png"},
+  ]);
+  const [activeChat, setActiveChat] = useState({});
+  const [user, setUser] = useState({
+    id:1234,
+    avatar: "https://www.w3schools.com/howto/img_avatar2.png",
+    name: "Usuario Logado"
+  });
 
- return (
-   <div className="app-window">
-     <div className="sidebar">
-        <header>
-            <img className="header--avatar" src={user.avatar} alt="" />
-            <div className="header--buttons">
-              <div className="header--btn">
-                <DonutLargeIcon style={{color: "#919191"}} />
-              </div>
-              <div className="header--btn">
-                <ChatIcon style={{color: "#919191"}} />
-              </div>
-              <div className="header--btn">
-                <MoreVertIcon style={{color: "#919191"}} />
-              </div>
-            </div>
-        </header>
-        <div className="search">
-            <div className="search--input">
-                <SearchIcon fontSize="small" style={{color: "#919191"}}/>
-                <input type="search" placeholder="Procurar ou começar uma nova conversa" />
-            </div>
-        </div>
-        <div className="chatlist">
-            {chatlist.map((item, key)=> (
-                <ChatListItem 
-                    key={key}
-                    data={item}
-                    active={activeChat.chatId === chatlist[key].chatId}
-                    onClick={()=>setActiveChat(chatlist[key])}
-                />
-            ))}
-        </div>
-     </div>
-     <div className="contentarea">
-        {activeChat.chatId !== undefined &&
-            <ChatWindow 
-                user={user}
-            />
-        }
+  const [showNewChat, setShowNewChat] = useState(false);
 
-        {activeChat.chatId === undefined &&
-            <ChatIntro />
-        }
-     </div>
-   </div>
- );
+  const handleNewChat = () => {
+    setShowNewChat(true);
+  }
+
+  return (
+    <div className="app-window">
+      <div className="sidebar">
+          <NewChat 
+              chatlist={chatlist}
+              user={user}
+              show={showNewChat}
+              setShow={setShowNewChat}
+          />
+          <header>
+              <img className="header--avatar" src={user.avatar} alt="" />
+              <div className="header--buttons">
+                <div className="header--btn">
+                  <DonutLargeIcon style={{color: "#919191"}} />
+                </div>
+                <div  onClick={handleNewChat} className="header--btn">
+                  <ChatIcon style={{color: "#919191"}} />
+                </div>
+                <div className="header--btn">
+                  <MoreVertIcon style={{color: "#919191"}} />
+                </div>
+              </div>
+          </header>
+          <div className="search">
+              <div className="search--input">
+                  <SearchIcon fontSize="small" style={{color: "#919191"}}/>
+                  <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+              </div>
+          </div>
+          <div className="chatlist">
+              {chatlist.map((item, key)=> (
+                  <ChatListItem 
+                      key={key}
+                      data={item}
+                      active={activeChat.chatId === chatlist[key].chatId}
+                      onClick={()=>setActiveChat(chatlist[key])}
+                  />
+              ))}
+          </div>
+      </div>
+      <div className="contentarea">
+          {activeChat.chatId !== undefined &&
+              <ChatWindow 
+                  user={user}
+              />
+          }
+
+          {activeChat.chatId === undefined &&
+              <ChatIntro />
+          }
+      </div>
+    </div>
+  );
 }
---
+++ src/components/NewChat/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import "./NewChat.css"
+
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+
+export default function NewChat({user, chatlist, show, setShow}) {
+
+    const [list,setList] = useState([
+        {id: 123,avatar: "https://www.w3schools.com/howto/img_avatar2.png",name: "Lucas Álvares"},
+        {id: 123,avatar: "https://www.w3schools.com/howto/img_avatar2.png",name: "Lucas Álvares"},
+        {id: 123,avatar: "https://www.w3schools.com/howto/img_avatar2.png",name: "Lucas Álvares"},
+        {id: 123,avatar: "https://www.w3schools.com/howto/img_avatar2.png",name: "Lucas Álvares"}
+    ]);
+
+    const handleClose = () => {
+        setShow(false);
+    }
+
+    return (
+    <div className="newChat" style={{left: show?0:-415}}>
+        <div className="newChat--head">
+                <div  onClick={handleClose} className="newChat--backbutton">
+                    <ArrowBackIcon style={{color: "#FFFFFF"}}/>
+                </div>
+                <div className="newChat--headtitle">Nova Conversa</div>
+        </div>
+        <div className="newChat--list">
+                {list.map((item, key)=>(
+                    <div className="newChat--item" key={key}>
+                        <img className="newChat--itemavatar" src={item.avatar} alt=""/>
+                        <div className="newChat--itemname">{item.name}</div>
+                    </div>
+                ))}
+        </div>
+    </div>
+    );
+}
